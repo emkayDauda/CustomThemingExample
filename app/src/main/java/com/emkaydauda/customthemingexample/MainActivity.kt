7f779a3bef3fc15a10264746e6c7e7e8b50e6ac2
Maaruf Dauda <mkdauda.work@gmail.com>
2020-02-26 17:44:09 +0100

    :sparkles: Create onClick function

--- app/src/main/java/com/emkaydauda/customthemingexample/MainActivity.kt
+++ app/src/main/java/com/emkaydauda/customthemingexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.emkaydauda.customthemingexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,13 @@ class MainActivity : AppCompatActivity() {
 //        getTheme()applyStyle(R.style.OverlayThemeLime, true) in Java
         theme.applyStyle(R.style.OverlayThemeLime, true)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view: View) {
+        when(view.id) {
+            R.id.lime -> {
+
+            }
+        }
     }
 }

073801d0d930d7b1b539e26ca2e200a806aec3d4
Maaruf Dauda <mkdauda.work@gmail.com>
2020-02-26 18:11:06 +0100

    :sparkles: Create sharePreference object and handle clicks

--- app/src/main/java/com/emkaydauda/customthemingexample/MainActivity.kt
+++ app/src/main/java/com/emkaydauda/customthemingexample/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.emkaydauda.customthemingexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var sharedPreferences: SharedPreferences
+    val themeKey = "currentTheme"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,13 +18,36 @@ class MainActivity : AppCompatActivity() {
 //        getTheme()applyStyle(R.style.OverlayThemeLime, true) in Java
         theme.applyStyle(R.style.OverlayThemeLime, true)
         setContentView(R.layout.activity_main)
+
+        sharedPreferences = getSharedPreferences(
+            "ThemePref",
+            Context.MODE_PRIVATE
+        )
+
+//        val sharedPreferencesEditor = sharedPreferences.edit()
     }
 
     fun onClick(view: View) {
         when(view.id) {
             R.id.lime -> {
-
+                sharedPreferences.edit().putString(themeKey, "lime").apply()
             }
+
+            R.id.red -> {
+                sharedPreferences.edit().putString(themeKey, "red").apply()
+            }
+
+            R.id.green-> {
+                sharedPreferences.edit().putString(themeKey, "green").apply()
+            }
+
+            R.id.blue -> {
+                sharedPreferences.edit().putString(themeKey, "blue").apply()
+            }
+
         }
+
+        startActivity(intent)
     }
+
 }

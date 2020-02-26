3ea28e3b3ac9a712021ef96fe649d37115638f60
Maaruf Dauda <mkdauda.work@gmail.com>
2020-02-26 17:34:31 +0100

    :sparkles: Override theme in activity

--- app/src/main/java/com/emkaydauda/customthemingexample/MainActivity.kt
+++ app/src/main/java/com/emkaydauda/customthemingexample/MainActivity.kt
@@ -7,6 +7,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        getTheme()applyStyle(R.style.OverlayThemeLime, true) in Java
+        theme.applyStyle(R.style.OverlayThemeLime, true)
         setContentView(R.layout.activity_main)
     }
 }
